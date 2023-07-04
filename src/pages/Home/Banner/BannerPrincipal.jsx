cd44b884ee65ac72019a40fb5ba6b69447ea8551
ciroDourado <ciro.brz@gmail.com>
2023-07-03 21:53:57 -0300

    refact: troca axios por fetch

--- src/pages/Home/Banner/BannerPrincipal.jsx
+++ src/pages/Home/Banner/BannerPrincipal.jsx
@@ -2,13 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { ContainerBanner } from './Style';
 import BtnExplorar from './ButtonExplorar/BtnExplorar';
 import { API_BASE_URL } from '../../../data/api/api';
-import api from '../../../services/api';
 
 export default function BannerPrincipal() {
   const [bannerData, setBannerData] = useState(null);
 
   useEffect(() => {
-    api.get(`api/banner-topos/1/?populate=*`)
+    fetch(`${API_BASE_URL}/api/banner-topos/1/?populate=*`)
       .then(response => response.json())
       .then(data => setBannerData(data.data.attributes))
       .catch(error => console.error(error));
