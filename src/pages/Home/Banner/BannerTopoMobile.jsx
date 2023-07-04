a99a55c4082edc6126c5212e7e9dd3e3e9c3ae63
ciroDourado <ciro.brz@gmail.com>
2023-07-03 21:59:16 -0300

    fix: troca fetch por axios

--- src/pages/Home/Banner/BannerTopoMobile.jsx
+++ src/pages/Home/Banner/BannerTopoMobile.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { BannerMobile, ContainerBannerMobile } from "./Style";
 import { API_BASE_URL } from '../../../data/api/api';
+import api from '../../../services/api';
 
 export default function BannerTopoMobile() {
   const [bannerData, setBannerData] = useState(null);
 
   useEffect(() => {
-    fetch(`${API_BASE_URL}/api/banner-topos/3/?populate=*`)
-      .then(response => response.json())
-      .then(data => setBannerData(data.data.attributes))
-      .catch(error => console.error(error));
+    api.get("/api/banner-topos/3/?populate=*")
+      .then(response => response.data.attributes)
+      .then(setBannerData)
+      .catch(console.error);
   }, []);
 
   if (!bannerData) {
