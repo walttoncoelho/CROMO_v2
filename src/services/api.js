0006ee371fdbc5ec49e96a122e5e550f81288d55
ciroDourado <ciro.brz@gmail.com>
2023-07-03 22:40:01 -0300

    refact: altera protocolo da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://adm.cromoconstrutora.com.br",
+  baseURL: "https://adm.cromoconstrutora.com.br",
 });
 
 export default api;
