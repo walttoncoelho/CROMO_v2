9aa7a5543689c801d950c88df0bf3ff81646244e
walttoncoelho <walltton@outlook.com>
2023-07-12 16:19:38 -0300

    Ajusta a seção nossos números

--- src/data/api/numeros.js
+++ src/data/api/numeros.js
@@ -1,5 +1,5 @@
 let numeros = { 
-  titulo: "Nossos numeros", 
+  titulo: "Nossos números", 
   descricao: "Em mais de 30 anos de história, a Cromo vem ajudando milhares de balsenses a realizar o sonho de seu lote ou casa própria e contribuindo no desenvolvimento do Sul do Maranhão..", 
   lotes: 1200, 
   asfalto: 2600, 
